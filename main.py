5a5a4f243a7f7ca3c7f928686078b3bd52fadb29
Sumir Vats <sumirvats@gmail.com>
2022-05-05 15:59:18 +0530

    Connected Database with contact form

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    msg = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(), nullable=True)
+
+    def __repr__(self):
+        return f"User {self.name}"
 
 @app.route("/")
 def index():
@@ -14,8 +29,18 @@ def about():
 def posts():
     return render_template('post.html')
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+	if(request.method=='POST'):
+		name = request.form.get('name')
+		email = request.form.get('email')
+		phone = request.form.get('phone')
+		message = request.form.get('message')
 
-app.run(debug=True)
+		entry = Contacts(name=name, phone=phone, email=email, msg=message, date=datetime.now())
+		db.session.add(entry)
+		db.session.commit()
+	return render_template('contact.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
